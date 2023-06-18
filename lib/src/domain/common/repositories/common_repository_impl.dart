11bca3ebe4948843744ead9d74e6349c4cfbeaf6
jmsandiegoo <jmsandiego0710@gmail.com>
2023-06-18 22:02:17 +0800

    updated interface for common repo

--- lib/src/domain/common/repositories/common_repository_impl.dart
+++ lib/src/domain/common/repositories/common_repository_impl.dart
@@ -1,9 +1,10 @@
-import 'package:dartz/dartz.dart';
 import 'package:launchlab/src/domain/common/models/category_entity.dart';
-import 'package:launchlab/src/utils/failure.dart';
+import 'package:launchlab/src/domain/common/models/skill_entity.dart';
 
 abstract class CommonRepositoryImpl {
-  /// Get categories
-  /// throws Failure
   Future<List<CategoryEntity>> getCategories();
+
+  Future<List<SkillEntity>> getSkillsInterestsFromEmsi(String? filter);
+
+  Future<String> getNewEmsiToken();
 }
