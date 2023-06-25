efb55410a4d4037dbc02c3fa113a0ef9f768bd5b
Shi Kang <50563302+Yskie@users.noreply.github.com>
2023-06-26 07:42:34 +0800

    Delete lib/config directory

--- lib/config/app_config.dart
+++
@@ -1,5 +0,0 @@
-/// Supabase setup
-
-const String supabaseUrl = "https://mqsbcgvpuigmaucqgxnc.supabase.co";
-const String supabaseToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xc2JjZ3ZwdWlnbWF1Y3FneG5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQyNDYyMTgsImV4cCI6MTk5OTgyMjIxOH0.LoqKLW-ZAbOduspASWlXiusiyD0074_Igy42yv0Ltik";
