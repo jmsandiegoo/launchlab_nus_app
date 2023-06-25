2e0b477a64d46a3096e5ab312be5ba7164dee1db
Shi Kang <50563302+Yskie@users.noreply.github.com>
2023-06-26 05:54:34 +0800

    Delete lib/domain directory

--- lib/domain/user/repositories/user_repository_impl.dart
+++
@@ -1,8 +0,0 @@
-import 'package:launchlab/src/domain/user/models/degree_programme_entity.dart';
-import 'package:launchlab/src/domain/user/models/requests/onboard_user_request.dart';
-
-abstract class UserRepositoryImpl {
-  Future<List<DegreeProgrammeEntity>> getDegreeProgrammes(String? filter);
-
-  Future<void> onboardUser({required OnboardUserRequest request});
-}
